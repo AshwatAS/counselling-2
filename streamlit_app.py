2bcd5d259cbb3f276603bea611b670b1b3a31af3
AshwatAS <63542367+AshwatAS@users.noreply.github.com>
2025-01-04 11:12:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ progress_bar_val=0
 with st.form("user_base_form"):
   user_name=st.text_input("Enter your name: ")
   user_age=st.number_input("Enter your age: ",min_value=15,step=1)
-  submit_button_bool=st.form_submit_button("Next",type="tertiary")
+  submit_button_bool=st.form_submit_button("Next",type="primary")
   
 if submit_button_bool:
   progress_bar_val+=1
