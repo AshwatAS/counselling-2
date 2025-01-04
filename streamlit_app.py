9b16df0fd9e69dde45e5f12fd8d68e5fb2277fdc
AshwatAS <63542367+AshwatAS@users.noreply.github.com>
2025-01-04 10:53:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,15 @@ import streamlit as st
 st.title('Counselling App')
 
 st.info('Form 1')
-user_name=st.text_input("Enter your name: ")
-user_age=st.number_input("Enter your age: ",min_value=15,step=1)
+
+int progress_bar_val=-1
+
+with st.form("user_base_form"):
+  user_name=st.text_input("Enter your name: ")
+  user_age=st.number_input("Enter your age: ",min_value=15,step=1)
+  submit_button_bool=st.form_submit_button("Next")
+  
+if submit_button_bool:
+  progress_bar_val+=1
+  
+st.progress(progress_bar_val,"Step",progress_bar_value+1,"of 5")
