712752c0c39abf9f4c01bb5eff671d9525fe689b
AshwatAS <63542367+AshwatAS@users.noreply.github.com>
2025-01-04 10:44:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ st.title('Counselling App')
 
 st.info('Form 1')
 user_name=st.text_input("Enter your name: ")
-user_age=st.number_input("Enter your age: ")
+user_age=st.number_input("Enter your age: ",min_value=15,step=1)
