da7b5e6356063e8d1659fd043a3fdbbc4bc7efc7
AshwatAS <63542367+AshwatAS@users.noreply.github.com>
2025-01-05 15:30:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,9 +28,11 @@ with st.form("user_base_form"):
         user_DOB = st.date_input("When's your birthday",format="DD.MM.YYYY",min_value=datetime.date(today_date.year-23,1,1),max_value=datetime.date(today_date.year-14,12,31))
         
         #EB=education board, SOE=Status of Education,EY=education year
-        user_soe=st.selectbox("What is your current year of education?",["Secondary","Higher Secondary","Undergraduate"])
-        if user_soe=="Secondary":
-                user_ey=st.selectbox("Which grade?",["9th","10th"])
+        user_soe=st.selectbox("What is your current year of education?",["Secondary","Higher Secondary","Undergraduate"],None)
+        if user_soe:
+                
+                if user_soe=="Secondary":
+                        user_ey=st.selectbox("Which grade?",["9th","10th"])
         
         user_eb=st.selectbox("What is your board of education?",["ICSE","CBSE","International","SSC","Others"],None)
         submit_button_bool=st.form_submit_button("Next",type="primary")
