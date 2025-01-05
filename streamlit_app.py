635e0d249e76052e6d12c09081bf13217cb6044d
AshwatAS <63542367+AshwatAS@users.noreply.github.com>
2025-01-05 11:27:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,16 +13,26 @@ st.info('Form 1')
 
 progress_bar_val=0
 today_date=datetime.date.today()
-st.write(today_date)
+
 if progress_bar_val not in st.session_state:
     st.session_state.progress_bar_val = 1
 
 with st.form("user_base_form"):
-  user_name=st.text_input("Enter your name: ",placeholder="Ex: Patrick Junes")
-  #user_age=st.number_input("Enter your age: ",min_value=15,step=1)
-  user_DOB = st.date_input("When's your birthday",format="DD.MM.YYYY",min_value=datetime.date(today_date.year-23,1,1),max_value=datetime.date(today_date.year-14,12,31))
-  submit_button_bool=st.form_submit_button("Next",type="primary")
-  
+        user_name=st.text_input("Enter your name: ",placeholder="Ex: Patrick Junes")
+#user_age=st.number_input("Enter your age: ",min_value=15,step=1)
+        user_DOB = st.date_input("When's your birthday",format="DD.MM.YYYY",min_value=datetime.date(today_date.year-23,1,1),max_value=datetime.date(today_date.year-14,12,31))
+        submit_button_bool=st.form_submit_button("Next",type="primary")
+
+if today_date.month<user_DOB.month:
+        user_age=today_date.year-user_DOB.year
+elif today_date.month>user_DOB.month:
+        user_age=today_date.year-user_DOB.year-1
+else:
+        if user_DOB.day<=today_date.day:
+                user_age=today_date.year-user_DOB.year-1
+        else:
+                user_age=today_date.year-user_DOB.year
+st.write(user_age)
 if submit_button_bool:
   progress_bar_val+=1
   st.switch_page("pages/page1.py")
