81dcd1c1746302fe68889b861df582652d253ed9
AshwatAS <63542367+AshwatAS@users.noreply.github.com>
2025-01-27 18:57:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,17 @@
 import streamlit as st
 import datetime
+import requests
+
+url = 'https://indian-cities-api-nocbegfhqg.now.sh/cities'
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    cities = response.json()
+    st.info(cities[1])  # Print the list of cities
+else:
+    st.info("Error:", response.status_code, response.text)
+
 
 st.set_page_config(page_title="Base form",initial_sidebar_state="collapsed",menu_items={
         'Get Help': 'https://www.youtube.com/',
