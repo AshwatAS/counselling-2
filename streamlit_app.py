dfc6e80923bbf0d109be4237acda08c47a345823
surekaamit7 <surekaamit7@gmail.com>
2025-01-28 17:44:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ user_gender = st.selectbox("What is your gender?", ["Male","Female"],None)
 user_DOB = st.date_input("When's your birthday",format="DD.MM.YYYY",min_value=datetime.date(today_date.year-23,1,1),max_value=datetime.date(today_date.year-14,12,31))
 user_category=st.selectbox("Choose Quota as may be applicable?",["SC","OBC","ST","General"],None)
 user_father_qualification=st.selectbox("What is your father's qualification?",["Chartered Accountant","Company Secretary","MBA","MBBS","IPS","IAS","Other"],None)
-user_mother_qualification=st.selectbox("What is your mother's qualification?",["CA","IIT","MBA","MBBS","IPS","IAS"],None)
+user_mother_qualification=st.selectbox("What is your mother's qualification?",["Chartered Accountant","Home Maker","MBA","MBBS","IPS","IAS","Other"],None)
 
 #EB=education board, SOE=Status of Education,EY=education year
 user_soe=st.selectbox("What is your current year of education?",["Secondary","Higher Secondary","Undergraduate"],None)
